affa1cdee346dfda9913f74d23177a96dd66dcf1
y4hy <usingthisforseriously@gmail.com>
2025-07-04 09:08:27 +0300

    save

--- software_installer.sh
+++ software_installer.sh
@@ -7,10 +7,11 @@ cp -R dotfiles/* ~/.dotfiles/
 
 stow fish kitty nvim rofi wp neofetch hyprland
 
+sudo chsh -s $(which fish)
+
 sudo pacman zen-browser dolphin zip unzip htop locate fuse3 rclone obsidian syncthing gnome-calculator openvpn gnome-keyring libsecret seahorse
 
 sudo pacman nodejs npm docker docker-compose nvidia-container-toolkit python-pip
 
 sudo pacman -S ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji
-
 
