a5d2811dbfa2b58bd63cd0896e842fce8bf60afc
y4hy <usingthisforseriously@gmail.com>
2025-07-05 11:30:42 +0300

    save

--- core_installer.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-
-pacman -Syu linux linux-lts linux-lts linux-lts-headers mkinitcpio openssh systemd-resolvconf
---
+++ installer.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/bash
+
+pacman -Syu linux-lts linux-lts-headers mkinitcpio openssh systemd-resolvconf
+
+pacman -S nvidia nvidia-dkms nvidia-utils nvidia-settings
+
+sudo cp nvidia/grub /etc/default/grub
+sudo cp nvidia/mkinitcpio /etc/mkinitcpio.conf
+
+sudo mkinitcpio -P
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo pacman -S pipewire pipewire-jack pipewire-alsa pipewire-pulse wireplumber
+
+mkdir ~/.dotfiles
+cp -R dotfiles/* ~/.dotfiles
+
+sudo pacman -S hyprland neovim kitty fish rofi swww stow
+
+cd ~/.dotfiles
+
+stow fish kitty nvim rofi wp neofetch hyprland
+
+chsh -s $(which fish)
+
+sudo pacman -S dolphin zip unzip htop locate fuse3 rclone obsidian wl-clipboard syncthing gnome-calculator openvpn gnome-keyring libsecret seahorse lxqt-sudo ttf-dejavu ttf-liberation noto-fonts noto-fonts-cjk noto-fonts-emoji nodejs npm docker docker-compose nvidia-container-toolkit python-pip
+
+sudo cp keyring/login /etc/pam.d/login
+
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
+
+sudo reboot now
