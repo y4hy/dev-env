bfa6e2ddf1efa3d344555fbf52fce29dfcbb22c9
Yahya <155630935+y4hy@users.noreply.github.com>
2025-09-14 23:19:15 +0300

    Fix command to add rust-analyzer component

--- installer.sh
+++ installer.sh
@@ -246,7 +246,7 @@ log_header "Installing Rust via rustup..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Add cargo to the current session's PATH. The shell rc file will handle future sessions.
 export PATH="$HOME/.cargo/bin:$PATH"
-rustc component add rust-analyzer
+rustup component add rust-analyzer
 echo "  -> Rust has been installed successfully."
 
 
